a2f4f4e7f2de71f1bec53ee1083b3ec8a1885b14
amitoh <amitoh@gmail.com>
2018-12-03 11:43:05 +0200

    initial 2

---
+++ patment-service/src/main/java/spring/cloud/app/userservice/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package spring.cloud.app.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("go")
+    public String go() {
+        return "payment service";
+    }
+
+}
---
+++ user-service/src/main/java/spring/cloud/app/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package spring.cloud.app.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("go")
+    public String go() {
+        return "user service";
+    }
+
+}
